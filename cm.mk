2e24c278a6b2e107d9fdde6b15e135e3f38471d0
mcgi5sr2 <mcgi5sr2@gmail.com>
2016-11-22 11:09:16 +0000

    not honami

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit AOSP Aries device parts
-$(call inherit-product, device/sony/honami/aosp_d5803.mk)
+$(call inherit-product, device/sony/aries/aosp_d5803.mk)
 
 # Inherit CM Shinano device parts
 $(call inherit-product, device/sony/shinano/platform2.mk)
@@ -19,6 +19,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5803
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5803/D5803:5.1.1/23.4.A.0.546/864948651:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5803-user 5.1.1 23.4.A.0.546 864948651 release-keys"
 
-# Override Product Name for crDroid
+# Override Product Name
 PRODUCT_NAME := cm_z3c
 PRODUCT_MODEL := Xperia Z3c
